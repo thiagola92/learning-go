02f30c6a3fff00ca438d11e2e68f7046878326b8
thiagola92 <thiagola92@gmail.com>
2024-04-11 19:12:55 -0300

    Backup

--- 03/main.go
+++ 03/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 
+	// Notice that I'm not using the file name ("example.go").
+	// Use the package name ("package my_package")
 	my_package "github.com/thiagola92/learning-go/02"
+	// Also notice that
+	// "https://github.com/thiagola92/learning-go/tree/main/02"
+	// Didn't work
 )
 
 func main() {
